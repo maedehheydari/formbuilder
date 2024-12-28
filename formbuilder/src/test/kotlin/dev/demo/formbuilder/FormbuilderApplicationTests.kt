0e71a9d9b4f02040c491e07da1ba27fbb217742f
maedehheydari <maedeh.heydari212@gmail.com>
2024-12-29 00:44:01 +0330

    first commit

---
+++ formbuilder/src/test/kotlin/dev/demo/formbuilder/FormbuilderApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.demo.formbuilder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FormbuilderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
